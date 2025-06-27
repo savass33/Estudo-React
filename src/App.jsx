d5ab6c35aa2a1a9b58ef9b5281389804297427d6
Savas <savas.neto20@gmail.com>
2025-06-27 16:06:56 -0300

    Estilização e inicio de taskCheck

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import Tasks from "./components/tasks";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Finalizar curso React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar ingles",
+      description: "Verbo to be",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskID) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskID) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Task Manager
         </h1>
-        <Tasks />
-        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,24 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className="bg-slate-400 text-left w-full text-slate-400 p-2 rounded-md"
+          >
+            {task.title}
+            {task.isCompleted ? "COMPLETO" : "INCOMPLETO"}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;

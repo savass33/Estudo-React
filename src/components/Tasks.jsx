d8613866a270215ddb3b6695f1d65c30a12fbaee
Savas <savas.neto20@gmail.com>
2025-07-02 14:03:28 -0300

    add: localStorage

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,14 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onTrashClick }) {
+  const navigate = useNavigate();
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -13,7 +21,10 @@ function Tasks({ tasks, onTaskClick, onTrashClick }) {
           >
             {task.title}
           </button>
-          <button className="text-slate-400 p-2 rounded-md">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="text-slate-400 p-2 rounded-md"
+          >
             <ChevronRightIcon />
           </button>
           <button
